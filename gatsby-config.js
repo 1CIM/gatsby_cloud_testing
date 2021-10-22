9c9501cc87d40cc57d8217a974c18c2ef63a4fa8
kim_haaga <kim.haaga@live.se>
2021-10-22 19:28:09 +0200

    Tut finnished

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,15 +1,26 @@
 module.exports = {
   siteMetadata: {
-    siteUrl: "https://www.yourdomain.tld",
-    title: "test site",
+    siteUrl: 'https://www.yourdomain.tld',
+    title: 'SKT1 FTW #Faker',
   },
   plugins: [
-    "gatsby-plugin-react-helmet",
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-plugin-react-helmet',
+    'gatsby-plugin-mdx',
+    'gatsby-transformer-sharp',
     {
-      resolve: "gatsby-plugin-manifest",
+      resolve: 'gatsby-plugin-manifest',
       options: {
-        icon: "src/images/icon.png",
+        icon: 'src/images/icon.png',
       },
     },
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: `blog`,
+        path: `${__dirname}/blog`,
+      }
+    },
   ],
-};
+}
